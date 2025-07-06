e436de8fa8b06330ca9417135f171d0dbacd0d66
111 <111>
2025-07-06 13:57:26 +0300

    Поменяла антидребезг кнопки, добавила режимы мигания диодов

--- main.c
+++ main.c
@@ -5,16 +5,12 @@
 #include "retarget.h"
 
 #define CNT_SAMPLE 5
-
 #define UART1_BAUD  115200
-
 
 #define GPIOA_ALL_Msk  0xFFFF
 #define GPIOB_ALL_Msk  0xFFFF
-
 #define LEDS_MSK  0xFF00
 #define PA5_MSK   (1 << 5)   // фонарик
-
 #define PA7_MSK   (1 << 7)   // кнопка
 #define LED0_MSK  (1 << 8)
 #define LED1_MSK  (1 << 9)
@@ -26,6 +22,15 @@
 #define LED7_MSK  (1 << 15)  // PA15
 
 char buff[120];
+
+// Режимы мигания диодов
+typedef enum LED_MODES{
+	MODE_RUNNING_LIGHT = 1,
+	MODE_BLINKING,
+	MODE_ALL_ON,
+	MODE_ALL_OFF,
+	MODES_COUNT
+};
 
 void TMR32_IRQHandler();
 
@@ -90,7 +95,6 @@ void UART1_init()
     uint32_t baud_icoef = HSECLK_VAL / (16 * UART1_BAUD);
     uint32_t baud_fcoef = ((HSECLK_VAL / (16.0f * RETARGET_UART_BAUD) - baud_icoef) * 64 + 0.5f);
 
-
     // Настраиваем GPIO
     RCU->CGCFGAHB_bit.GPIOAEN = 1;  // включает такирование AHB
     RCU->RSTDISAHB_bit.GPIOAEN = 1; // снимает сброс
@@ -116,10 +120,9 @@ void UART1_init()
     // включает тактирование
     RCU->UARTCLKCFG[1].UARTCLKCFG_bit.CLKEN = 1;
 
-    // Fractional Baud Rate Divisor
     UART1->IBRD = baud_icoef; // записывасет дробную часть
 
-    // Line Control Register High
+    // Fractional Baud Rate Divisor
     UART1->FBRD = baud_fcoef;
 
     // Line Control Register High
@@ -143,19 +146,22 @@ void UART1_init()
 //-- Peripheral init functions -------------------------------------------------
 void periph_init()
 {
-	//BSP_led_init();
+	BSP_led_init();
 	SystemInit();
 	SystemCoreClockUpdate();
-	//BSP_led_init();
-	//retarget_init();
+	BSP_led_init();
 	UART1_init();
-	// printf("K1921VG015 SYSCLK = %d MHz\n", (int)(SystemCoreClock / 1E6));
-	// printf("  UID[0] = 0x%X  UID[1] = 0x%X  UID[2] = 0x%X  UID[3] = 0x%X\n", (unsigned int)PMUSYS->UID[0], (unsigned int)PMUSYS->UID[1], (unsigned int)PMUSYS->UID[2], (unsigned int)PMUSYS->UID[3]);
-	// printf("  Start RunLeds\n");
+	retarget_init(); // перенаправление стандартного printf
+
+	printf("K1921VG015 SYSCLK = %d MHz\n", (int)(SystemCoreClock / 1E6));
+	printf("  UID[0] = 0x%X  UID[1] = 0x%X  UID[2] = 0x%X  UID[3] = 0x%X\n", (unsigned int)PMUSYS->UID[0], (unsigned int)PMUSYS->UID[1], (unsigned int)PMUSYS->UID[2], (unsigned int)PMUSYS->UID[3]);
+	printf("  Start RunLeds\n");
 }
 
 //--- USER FUNCTIONS ----------------------------------------------------------------------
 volatile uint32_t led_shift;
+volatile uint32_t button_click_counter = 0;
+
 
 //-- Main ----------------------------------------------------------------------
 int main(void)
@@ -163,30 +169,113 @@ int main(void)
 	uint32_t	i;
 	periph_init();
 	// TMR32_init(SystemCoreClock>>10);  // если << 1, медленно
-	//TMR32_init(500000); // 50 000 000 частота ядра, 500 000 10мс
+	TMR32_init(500000); // 50 000 000 частота ядра, 500 000 10мс
 	InterruptEnable();
 	led_shift = LED0_MSK;
 	while (1)
 	{
-		UART1->DR = 0x053;
-	    for(i=0;i<100000; ++i)
-	    {}
+		//UART1->DR = 0x053;
+	   // for(i=0;i<100000; ++i)
+	   // {}
 	}
 
 	return 0;
 }
 
+
+void update_leds()
+{
+	switch(button_click_counter)
+	{
+	case MODE_RUNNING_LIGHT:
+		GPIOA->DATAOUTTGL = led_shift;
+		led_shift = led_shift << 1;
+		if(led_shift > LED7_MSK) led_shift = LED0_MSK;
+		break;
+
+	case MODE_BLINKING:
+		GPIOA->DATAOUTTGL = LEDS_MSK;
+		break;
+
+	case MODE_ALL_ON:
+		GPIOA->DATAOUTSET = LEDS_MSK;
+		break;
+
+	case MODE_ALL_OFF:
+		GPIOA->DATAOUTCLR = LEDS_MSK;
+		break;
+
+	default:
+		break;
+	}
+}
+
 //-- IRQ INTERRUPT HANDLERS ---------------------------------------------------------------
+// не проверяла на мк
+void TMR32_IRQHandler()
+{
+	static uint8_t d = 0;
+	static uint8_t last_button_state = 1;
+	static uint32_t last_led_update = 0;
+
+	uint8_t x = (GPIOA->DATA & PA7_MSK) ? 1 : 0;
+	static float Ysm = 1000.0f;
+	const float k = 0.2f;
+
+	Ysm = Ysm + k * (x * 1000.0f - Ysm);
+
+	if(Ysm > 1000.0f) Ysm = 1000.0f;
+	else if(Ysm < 0.0f) Ysm = 0.0f;
+
+	// 1 отпущена, 0 нажата
+	static uint8_t button_state = 1;
+	static uint8_t prev_button_state = 1;
+
+	if (Ysm > 700)
+	{
+		button_state = 1;
+	}
+	else if (Ysm < 300)
+	{
+		button_state = 0;
+	}
+
+
+	if (button_state == 0 && prev_button_state == 1)
+	{
+		if(button_click_counter < (MODES_COUNT - 1))
+		{
+			button_click_counter++;
+		}
+		else
+		{
+			button_click_counter = 1; // в первый режим
+		}
+	}
+
+	prev_button_state = button_state;
+
+	if (++last_led_update >= 50) // чтобы обновление состояния было плавным
+		{
+			update_leds();
+			last_led_update = 0;
+		}
+
+	TMR32->IC = 3;
+}
+
+/* второй вариант без интегральной схемы
 void TMR32_IRQHandler()
 {
 	static uint8_t d = 0;
 	static uint8_t last_button_state = 1;
 	uint8_t curr_button_state = (GPIOA->DATA & PA7_MSK) ? 1 : 0;
-
+	static uint32_t last_led_update = 0;
 
 	if (curr_button_state != last_button_state)
 	{
 		d = 5;
+
 	}
 	else if (d > 0)
 	{
@@ -195,20 +284,28 @@ void TMR32_IRQHandler()
 		// если счётчик дошёл до 0 и кнопка нажата
 		if (d == 0 && curr_button_state == 0)
 		{
-			GPIOA->DATAOUTTGL = PA5_MSK; // PA5_MSK - фонарик, LED7_MSK - диод
+			//   GPIOA->DATAOUTTGL = LED7_MSK; // PA5_MSK - фонарик, LED7_MSK - диод
 			//Send_buff("Button pressed\n");
+
+			if(button_click_counter < (MODES_COUNT - 1))
+			{
+				button_click_counter++;
+			}
+			else
+			{
+				button_click_counter = 1; // в первый режим
+			}
 		}
+	}
+
+	if (++last_led_update >= 50) // чтобы обновление состояния было плавным
+	{
+		update_leds();
+		last_led_update = 0;
 	}
 
 	last_button_state = curr_button_state;
 
-	//Сбрасываем флаг прерывания таймера
 	TMR32->IC = 3;
 }
-
-// GPIOA->DATAOUTTGL = led_shift;
-// led_shift = led_shift << 1;  // бегающий огонек
-// if(led_shift > LED7_MSK) led_shift = LED0_MSK;
-//GPIOA->DATAOUTTGL = led_shift;
-// GPIOA->DATAOUTTGL = LED7_MSK;  // 12
-//GPIOA->DATAOUT = (LED4_MSK | LED5_MSK);  // горят оба диода
+ */
